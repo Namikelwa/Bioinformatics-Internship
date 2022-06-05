b69c8b6ac802145d13c63d1039f73c131c9f76f2
Dorcus Namikelwa <85280529+Namikelwa@users.noreply.github.com>
2022-06-05 21:12:11 +0300

    Create Htseq.sh

---
+++ RNA-SEQ/Htseq.sh
@@ -0,0 +1,6 @@
+
+htseq-count -t exon -i gene_id -f bam <input bam file/s>  <input gff file>  > <output file with a txt extension>
+#-t featuretype
+#-i attribute
+#-f format
+#a wild card can be used for multiple bam files
